79a791332c80124d04d1aa58c4cb7632617f2f8a
mikalumfernando <kalumsneha@yahoo.com>
2024-11-01 05:00:02 +0100

    Initial Commit

---
+++ src/main/java/com/ontariotechu/sdmt/learnlo_backend/LearnloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ontariotechu.sdmt.learnlo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnloBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnloBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ontariotechu/sdmt/learnlo_backend/LearnloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ontariotechu.sdmt.learnlo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnloBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
