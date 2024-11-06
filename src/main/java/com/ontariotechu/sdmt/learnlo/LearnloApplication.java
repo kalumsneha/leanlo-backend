482456696a3ac25734bd6afd93ab84e00d976121
mikalumfernando <kalumsneha@yahoo.com>
2024-11-06 05:12:27 +0100

    Basic Skeleton

--- src/main/java/com/ontariotechu/sdmt/learnlo/LearnloApplication.java
+++ src/main/java/com/ontariotechu/sdmt/learnlo/LearnloApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnloBackendApplication {
+public class LearnloApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnloBackendApplication.class, args);
+		SpringApplication.run(LearnloApplication.class, args);
 	}
 
 }
